5b55830850ba2100e9241421994c4e1b65664cb5
ShrideviReddy <shridevi.reddy13@gmail.com>
2022-05-27 06:04:41 -0400

    added code for operators

---
+++ basic_operators/basic_operators.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace  basic_operators{
+
+
+	class basic_operators{
+	
+	
+		static void Main(string[] args)
+		{
+		
+		// Add operation
+		int sum = 5 + 5;
+
+		// Substract operation
+		int minus = 5 - 3;
+
+		// Multiplication operation
+		int product = 5 * 3;
+
+		//Divide operation
+		float quotient = 4/2;
+
+		Console.WriteLine("Operation Results");
+
+		Console.WriteLine("5 + 5: " + sum);
+		Console.WriteLine("5 - 3: " + minus);
+		Console.WriteLine("5 * 3: " + product);
+		Console.WriteLine("4 / 2: " + quotient);
+		}	
+	
+	}
+
+}
