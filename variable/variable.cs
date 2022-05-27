f7ce5de9f25d6107f0606d6906b32861be2c2687
ShrideviReddy <shridevi.reddy13@gmail.com>
2022-05-26 22:39:24 -0400

    first commit

---
+++ variable/variable.cs
@@ -0,0 +1,18 @@
+using System;
+
+class practise_variable{
+
+static void Main(string[] args){
+
+
+	int a = 2;
+	string name = "Dave";
+	double pi = 3.14;
+	Console.WriteLine("Value of a:" +a);
+	Console.WriteLine("Value of name:"+ name);
+	Console.WriteLine("Value of pi:"+ pi);
+
+}
+
+
+}
