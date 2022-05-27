3cfd5a0061e818dbb23fd4bafd597d5843bd86a9
ShrideviReddy <shridevi.reddy13@gmail.com>
2022-05-27 05:54:35 -0400

    added user input code

---
+++ userinput/user_input.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace userinput{
+
+	class userinput
+	{
+	
+	
+	static void Main(string[] args)
+	{
+	
+	//Type your username and press enter
+	  Console.WriteLine("Enter username");
+        
+
+        // Create string variable and get user input
+	  string userName = Console.ReadLine();
+       
+        // Print the value of variable 
+	 Console.WriteLine("Username is: "+ userName);	  
+	
+	
+	
+	}
+	
+	
+	}
+
+}
